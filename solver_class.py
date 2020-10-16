26544e746e3d7d862c5b315439bc706e6cd6e4a7
Oleg Semenov <53193079+ilmonyd@users.noreply.github.com>
2020-10-16 13:23:22 +0200

    Delete solver_class.py

--- solver_class.py
+++
@@ -1,7 +0,0 @@
-class cell():
-    def _init_(self, pos) :
-        self.possibleAnswers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.answer = None
-        self.position = pos
-        self.solved = False
-        
